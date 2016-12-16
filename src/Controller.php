d8d2be4a9ea4049dba08da827225d5a01299933f
José Henrique Honjoya <ti.honjoya@gmail.com>
2016-12-16 08:37:13 -0200

    Fixed PotgreSQL, error with grouping
    Fixed bellow error:
    
    SQLSTATE[42803]: Grouping error: 7 ERROR: column "ltm_translations.id" must appear in the GROUP BY clause or be used in an aggregate function

--- src/Controller.php
+++ src/Controller.php
@@ -24,7 +24,7 @@ class Controller extends BaseController
             $groups->whereNotIn('group', $excludedGroups);
         }
 
-        $groups = $groups->get()->pluck('group', 'group');
+        $groups = $groups->select('group')->get()->pluck('group', 'group');
         if ($groups instanceof Collection) {
             $groups = $groups->all();
         }
