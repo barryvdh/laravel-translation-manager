70169fcab053933d955f6b9efa1155d68e84d3eb
Derek MacDonald <derekmd@hotmail.com>
2017-02-26 08:51:59 -0500

    Add export 'sort_keys' config option (#178)
    * Refactor query building to Translation scopes
    
    * Add 'sort_keys' config option
    
    Allows export translation to sort the
    output arrays with keys sorted ascending
    to make version control diff operations
    less arbitrary.

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -22,4 +22,33 @@ class Translation extends Model{
     protected $table = 'ltm_translations';
     protected $guarded = array('id', 'created_at', 'updated_at');
 
+    public function scopeOfTranslatedGroup($query, $group)
+    {
+        return $query->where('group', $group)->whereNotNull('value');
+    }
+
+    public function scopeOrderByGroupKeys($query, $ordered) {
+        if ($ordered) {
+            $query->orderBy('group')->orderBy('key');
+        }
+
+        return $query;
+    }
+
+    public function scopeSelectDistinctGroup($query)
+    {
+        $select = '';
+
+        switch (DB::getDriverName()){
+            case 'mysql':
+                $select = 'DISTINCT `group`';
+                break;
+            default:
+                $select = 'DISTINCT "group"';
+                break;
+        }
+
+        return $query->select(DB::raw($select));
+    }
+
 }
