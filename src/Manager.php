f243c17d5cd0699c578f8700075b2d9ddd882cb0
Márlon Etiene <marlon@eaglesistemas.com.br>
2017-03-21 16:26:25 -0300

    Added a slash as separator to iterate with subdirectories when use find command.

--- src/Manager.php
+++ src/Manager.php
@@ -104,7 +104,7 @@ class Manager{
             "[\'\"]".                           // Match " or '
             "(".                                // Start a new group to match:
                 "[a-zA-Z0-9_-]+".               // Must start with group
-                "([.][^\1)]+)+".                // Be followed by one or more items/keys
+                "([.|\/][^\1)]+)+".             // Be followed by one or more items/keys
             ")".                                // Close group
             "[\'\"]".                           // Closing quote
             "[\),]";                            // Close parentheses or new parameter
