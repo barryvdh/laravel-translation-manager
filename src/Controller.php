0ab8ec33547b7c8419c5019ec477472f842e5a37
Simeria Ionut <simeriaionut@gmail.com>
2017-04-18 13:37:51 +0300

    Fixed $key argument not being passed to the postDelete method, therefore running into a 500 Internal Server Error. (#191)

--- src/Controller.php
+++ src/Controller.php
@@ -102,7 +102,7 @@ class Controller extends BaseController
         }
     }
 
-    public function postDelete($group = null)
+    public function postDelete($group = null, $key)
     {
         if(!in_array($group, $this->manager->getConfig('exclude_groups')) && $this->manager->getConfig('delete_enabled')) {
             Translation::where('group', $group)->where('key', $key)->delete();
