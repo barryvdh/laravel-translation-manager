9c5481be27ccf1544be1b401150d5493c5805fb9
Stefan Ninic <ninicstefan94@gmail.com>
2018-05-29 14:37:15 +0200

    Enable adding custom translation functions, because you might make cu… (#265)
    * Enable adding custom translation functions, because you might make custom Vue.js trans function and use it in Vue files, translation manager won't recognise those.
    
    Eg. when using this method https://github.com/rmariuzzo/laravel-localization-loader you end up with something like trans.get where get is required in any case you name your translator.
    
    * Wrong config key for trans_functions
    
    * Dispatching events when exporting translations

--- config/translation-manager.php
+++ config/translation-manager.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
 
     /*
     |--------------------------------------------------------------------------
@@ -10,47 +10,60 @@ return array(
     | The default group settings for the elFinder routes.
     |
     */
-    'route' => [
-        'prefix' => 'translations',
+    'route'          => [
+        'prefix'     => 'translations',
         'middleware' => 'auth',
     ],
 
-	/**
-	 * Enable deletion of translations
-	 *
-	 * @type boolean
-	 */
-	'delete_enabled' => true,
+    /**
+     * Enable deletion of translations
+     *
+     * @type boolean
+     */
+    'delete_enabled' => true,
 
-	/**
-	 * Exclude specific groups from Laravel Translation Manager. 
-	 * This is useful if, for example, you want to avoid editing the official Laravel language files.
-	 *
-	 * @type array
-	 *
-	 * 	array(
-	 *		'pagination',
-	 *		'reminders',
-	 *		'validation',
-	 *	)
-	 */
-	'exclude_groups' => array(),
-    
+    /**
+     * Exclude specific groups from Laravel Translation Manager.
+     * This is useful if, for example, you want to avoid editing the official Laravel language files.
+     *
+     * @type array
+     *
+     *    array(
+     *        'pagination',
+     *        'reminders',
+     *        'validation',
+     *    )
+     */
+    'exclude_groups' => [],
+
     /**
      * Exclude specific languages from Laravel Translation Manager.
      *
      * @type array
      *
-     * 	array(
-     *		'fr',
-     *		'de',
-     *	)
+     *    array(
+     *        'fr',
+     *        'de',
+     *    )
      */
-    'exclude_langs' => array(),
+    'exclude_langs'  => [],
 
-	/**
-	 * Export translations with keys output alphabetically.
-	 */
-	'sort_keys ' => false,
+    /**
+     * Export translations with keys output alphabetically.
+     */
+    'sort_keys '     => false,
 
-);
+    'trans_functions' => [
+        'trans',
+        'trans_choice',
+        'Lang::get',
+        'Lang::choice',
+        'Lang::trans',
+        'Lang::transChoice',
+        '@lang',
+        '@choice',
+        '__',
+        '$trans.get',
+    ],
+
+];
