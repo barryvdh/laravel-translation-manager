1c8c5840aa09d564bc5a20ed1aac574cef1bd47f
charlizesmith <charlizesmithweb@gmail.com>
2020-02-07 16:44:57 +0530

    Update Controller.php

--- src/Controller.php
+++ src/Controller.php
@@ -60,7 +60,7 @@ class Controller extends BaseController
             ->with('group', $group)
             ->with('numTranslations', $numTranslations)
             ->with('numChanged', $numChanged)
-            ->with('editUrl', action('\Barryvdh\TranslationManager\Controller@postEdit', [$group]))
+            ->with('editUrl', $group ? action('\Barryvdh\TranslationManager\Controller@postEdit', [$group]) : null)
             ->with('paginationEnabled', $this->manager->getConfig('pagination_enabled'))
             ->with('deleteEnabled', $this->manager->getConfig('delete_enabled'));
     }
