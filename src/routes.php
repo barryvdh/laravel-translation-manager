f55962a5eef249442ddc46f6806c288b641dd745
Gazder Bence <bencegazder@gmail.com>
2020-10-25 17:32:39 +0100

    Fix api routes

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 $config = array_merge(config('translation-manager.route'), ['namespace' => 'Barryvdh\TranslationManager']);
-Route::group($config, function ($router) use ($config) {
+
+Route::group($config, function ($router) {
     $router->get('/locales/download', 'Controller@downloadLangFiles');
     $router->get('view/{groupKey?}', 'Controller@getView')->where('groupKey', '.*');
     $router->get('/{groupKey?}', 'Controller@getIndex')->where('groupKey', '.*');
@@ -17,14 +18,14 @@ Route::group($config, function ($router) use ($config) {
     $router->post('/locales/remove', 'Controller@postRemoveLocale');
     $router->post('/publish/{groupKey}', 'Controller@postPublish')->where('groupKey', '.*');
     $router->post('/translate-missing', 'Controller@postTranslateMissing');
-
-    // Translation API routes
-    if (config('translation-manager.api_endpoints_enabled', false)) {
-        $apiConfig = array_merge($config, config('translation-manager.api_route', []));
-
-        $router->group($apiConfig, function ($router) {
-            $router->get('/locales', 'Controller@getLocales');
-            $router->get('/locales/{slug}', 'Controller@getLocaleTranslations');
-        });
-    }
 });
+
+// Translation API routes
+if (config('translation-manager.api_endpoints_enabled', false)) {
+    $apiConfig = array_merge($config, config('translation-manager.api_route', []));
+
+    Route::group($apiConfig, function ($router) use ($apiConfig) {
+        $router->get('/locales', 'Controller@getLocales');
+        $router->get('/locales/{slug}', 'Controller@getLocaleTranslations');
+    });
+}
