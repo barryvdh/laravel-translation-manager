1a0420bafb8be1247e728b2c7fc572700b84918e
HarryThe3rd <harry.werkmeister@gmail.com>
2022-04-29 14:39:40 +0200

    fix #406 translation export for groups this was introduced with the fix for #417

--- src/Manager.php
+++ src/Manager.php
@@ -252,7 +252,6 @@ class Manager
 
     public function exportTranslations($group = null, $json = false)
     {
-        $group = basename($group);
         $basePath = $this->app['path.lang'];
 
         if (! is_null($group) && ! $json) {
